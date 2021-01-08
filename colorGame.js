30a622d56e49f0b151b02bf0c579eec98e818b2e
yasserkhan45 <74680702+yasserkhan45@users.noreply.github.com>
2021-01-09 01:03:20 +0530

    Create colorGame.js

---
+++ colorGame.js
@@ -0,0 +1,39 @@
+var colors = ["rgb(255, 0, 0)",
+				"rgb(255, 255, 0)",
+				"rgb(0, 255, 0)",
+				"rgb(255, 0, 255)",
+				"rgb(0, 0, 255)",
+				"rgb(0, 255, 255)",
+			]
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	squares[i].style.backgroundColor = colors[i];  
+
+	squares[i].addEventListener("click", function(){
+		var clickedColor = this.style.backgroundColor;
+		if(clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
+		}
+		else{
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	});
+}
+
+function changeColors(color){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
